636fcfce65a97d8e3761863fe93a3cb8ef80ae8a
Dawid Głowiak <74562355+dawidglowiak96@users.noreply.github.com>
2021-12-27 23:16:11 +0100

    Add files via upload

---
+++ projekt/projekt/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pos pos = new Pos();
+            pos.DefaultInvnt();
+            pos.Begin();            
+
+        }
+    }
+}
